a5563244bed106e2a720c556da898ee32cd76649
Exandeor <aao.workaholic@gmail.com>
2022-10-14 20:41:31 +0630

    2ndWhatever

---
+++ NewFile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+function NewFile(props) {
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+export default NewFile;
